52ea6af16cc6bb90ceb5640c72c3e5f18a72ec09
Yash Walia <yashbest005@gmail.com>
2020-09-05 19:38:47 +0530

    Updates

---
+++ precache-manifest.c51cac0a06d6e6f81e9a61369c8e9057.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2630e6b88a9839dbd3a2cf8fc11530",
+    "url": "/breaking-bad-react/index.html"
+  },
+  {
+    "revision": "2db38dff157dc7671741",
+    "url": "/breaking-bad-react/static/css/main.2f676849.chunk.css"
+  },
+  {
+    "revision": "8ac9832ab506a054ec6e",
+    "url": "/breaking-bad-react/static/js/2.1feeffcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-react/static/js/2.1feeffcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db38dff157dc7671741",
+    "url": "/breaking-bad-react/static/js/main.88cbbd30.chunk.js"
+  },
+  {
+    "revision": "32a6431dc58f01ee00a2",
+    "url": "/breaking-bad-react/static/js/runtime-main.5ab63ded.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-react/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-react/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-react/static/media/spinner.235b9b3e.gif"
+  }
+]);
