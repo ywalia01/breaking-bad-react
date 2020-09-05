0008b5013881c12f63d0858a03278ee550f172a4
Yash Walia <yashbest005@gmail.com>
2020-09-05 19:32:35 +0530

    initial commit

---
+++ src/components/ui/Theme.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Theme = () => {
+  return (
+    <div className="center" style={{ margin: "30px" }}>
+      <iframe
+        src="https://open.spotify.com/embed/track/2hsLpiKNkWpd4e9QuVdhar"
+        width="300"
+        height="380"
+        frameborder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Theme;
